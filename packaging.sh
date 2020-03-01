aca5aad62d8eaec20e82f136facaf26c45ddb3a1
nanato12 <admin@nanato12.info>
2020-03-02 02:34:47 +0900

    Modify packaging.sh ・マージの自動化

--- packaging.sh
+++ packaging.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+git pull
+
 apt-ftparchive packages ./debs > Packages
 bzip2 -k Packages
 gzip -c Packages > Packages.gz
@@ -7,3 +9,10 @@ gzip -c Packages > Packages.gz
 git add .
 git commit -m "Modify Packages File"
 git push
+
+git checkout master
+git pull
+
+git merge cydia_repo_develop
+
+git checkout cydia_repo_develop
