3341dd5c0e66647026667eaf28b9c3d481b98bde
nanato12 <admin@nanato12.info>
2020-03-02 02:51:22 +0900

    Modify packaging.sh ・自動マージの削除

--- packaging.sh
+++ packaging.sh
@@ -9,10 +9,3 @@ gzip -c Packages > Packages.gz
 git add .
 git commit -m "Modify Packages File"
 git push
-
-git checkout master
-git pull
-
-git merge cydia_repo_develop
-
-git checkout cydia_repo_develop
